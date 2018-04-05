412fab1df1073c40ff2d8f07e8abdc53f6eda9fc
bretzelus <lussier.serge@gmail.com>
2018-04-05 18:55:39 -0400

    sync

--- NasCloud.sh
+++ NasCloud.sh
@@ -3,13 +3,13 @@
 if [ "$2" = "up" ]; then
 
     mount -t nfs netfs:/nfs/bretzelus  /netfs/bretzelus
-    mount -t nfs netfs:/nfs/Public  /netfs/public
+    mount -t nfs netfs:/nfs/Public  /netfs/shared
     mount -t nfs netfs:/nfs/xilef  /netfs/xilef
 
 elif [ "$2" = "down" ]; then
 
     umount /netfs/bretzelus
-    umount /netfs/public
+    umount /netfs/shared
     umount /netfs/xilef
 
 fi
